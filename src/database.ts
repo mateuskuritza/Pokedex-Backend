e1ef85d2130048317b23568546b7777d7e446d2a
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-08-05 20:49:04 -0300

    heroku configs?

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,4 @@
 import { getConnectionManager } from "typeorm";
-/*
-if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL.indexOf("sslmode=require") === -1) {
-  process.env.DATABASE_URL += "?sslmode=require";
-}*/
 
 export default async function connect() {
     const connectionManager = await getConnectionManager();
@@ -11,7 +7,9 @@ export default async function connect() {
         type: "postgres",
         url: process.env.DATABASE_URL,
         entities: [`${process.env.NODE_ENV === 'production' ? 'dist' : 'src'}/entities/*.*`],
-        ssl: process.env.NODE_ENV === 'production'
+        ssl: {
+            rejectUnauthorized: false
+        }
     });
     await connection.connect();
     return connection;
