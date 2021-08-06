7336eec8346c7becc4f855da967804fe7d3dc029
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-08-06 20:53:12 -0300

    live chat with socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,25 @@
-import app, { init } from "./app";
+import httpServer, { init } from "./app";
+const socket = require('socket.io');
 
 const port = +process.env.PORT || 4000;
 
-init().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}.`);
-  });
+const server = init().then(() => {
+    httpServer.listen(port, () => {
+        console.log(`Listening on port ${port}`)
+    });
+});
+
+const io = socket(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-allowed"],
+        credentials: false
+    }
+});
+
+io.on("connection", async (socket: any) => {
+    socket.on("newMessage", () => {
+        socket.broadcast.emit("loadMessages");
+    });
 });
